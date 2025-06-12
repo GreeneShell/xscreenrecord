5e9541d7ff49d78abc3f21e85154f9424658844c
xieguozhu <xxxiegz@163.com>
2025-06-12 08:26:59 +0800

    Initial Commit

---
+++ xscreenrecord/xscreenrecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  xscreenrecordApp.swift
+//  xscreenrecord
+//
+//  Created by apple on 2025/6/12.
+//
+
+import SwiftUI
+
+@main
+struct xscreenrecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xscreenrecordTests/xscreenrecordTests.swift
@@ -0,0 +1,17 @@
+//
+//  xscreenrecordTests.swift
+//  xscreenrecordTests
+//
+//  Created by apple on 2025/6/12.
+//
+
+import Testing
+@testable import xscreenrecord
+
+struct xscreenrecordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
